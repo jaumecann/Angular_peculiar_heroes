fd79e2b8cfc592cfef83dbf6af4d9616693965f8
Jaume <jcanals@haley.es>
2020-03-20 15:05:42 +0100

    commit inicial

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero ;
+  heroes: Hero [] = HEROES;
+
+  selectHero(whatever: Hero): void {
+    this.hero = whatever;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero [] = [
+    { id: 11, name: 'Perro'},
+    { id: 12, name: 'Avestruz'},
+    { id: 13, name: 'Babuino'},
+    { id: 14, name: 'Erizo'},
+    { id: 15, name: 'Culebra'},
+    { id: 16, name: 'Herrerillo'},
+    { id: 17, name: 'Rinoceronce'},
+    { id: 18, name: 'Fenec'},
+    { id: 19, name: 'Libelula'},
+    { id: 20, name: 'Canguro'},
+]
